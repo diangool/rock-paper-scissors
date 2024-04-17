ab209aff2deeeda3ec79ab7f35214fab3c51efb8
diangool <diangoold@proton.me>
2024-04-17 11:44:34 -0500

    Add the project's files, and firs function to randomly select rock,paper or scissors

---
+++ index.js
@@ -0,0 +1,6 @@
+function getComputerChoice(Rock, Paper, Scissors) {
+    const options = ['Rock', 'Paper', 'Scissors'];
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
+console.log(getComputerChoice());
